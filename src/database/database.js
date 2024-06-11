9ede65916b47d49ff105b12961b7d0124c9670f1
develoddy <eddylujann@gmail.com>
2024-06-11 10:53:09 +0000

    fix

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize }  from "sequelize";
 
-export const sequelize = new Sequelize( 'ecommercedb', 'root', '', {
-    host: "localhost",
+export const sequelize = new Sequelize( 'ecommercedb', 'lujandev', '$$Sistemas201290', {
+    host: "mysql-8001.dinaserver.com",
     dialect: "mysql",
     define: {
           timestamps: false,
