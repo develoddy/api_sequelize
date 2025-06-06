f49021f292c753a8ad1024d17c64a599d2b5aa9e
Eddy Lujan <eddylujann@gmail.com>
2025-06-06 15:59:57 +0200

    fix: databae

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,15 @@
 import { Sequelize }  from "sequelize";
+import dotenv from "dotenv";
 
-export const sequelize = new Sequelize( 'ecommercedb', 'root', '', {
-    host: "localhost",
-    dialect: "mysql",
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     define: {
-          timestamps: false,
+      timestamps: false,
     },
-})
+  }
+);
