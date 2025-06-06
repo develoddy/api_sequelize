3e75ba5b06362f0be1f397898a730a27f53a303b
Eddy Lujan <eddylujann@gmail.com>
2025-06-06 16:06:12 +0200

    fix: databae

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,6 @@
 import { Sequelize }  from "sequelize";
-import dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
