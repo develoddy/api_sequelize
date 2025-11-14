312bfa9f4c2f9211bbb9ad3f77f4b9f0d737ee3a
Eddy Lujan <eddylujann@gmail.com>
2025-11-14 16:14:56 +0100

    💾 Pre-Deploy API 2025-11-14 16:14:56

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "api_sequelize",
-      script: "src/index.js",
-      instances: 1,
-      exec_mode: "fork",
-      autorestart: true,
+      name: 'api_sequelize',
+      script: './index.js',
       watch: false,
-      max_memory_restart: "300M",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3500,
-      }
-    }
-  ]
+    },
+  ],
 };
+
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "api_sequelize",
+//       script: "src/index.js",
+//       instances: 1,
+//       autorestart: true,
+//       watch: false,
+//       max_memory_restart: "300M",
+//       env: {
+//         NODE_ENV: "production",
+//         PORT: 3500
+//       }
+//     }
+//   ]
+// };
