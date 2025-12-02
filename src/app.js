a82a687463dd6ad7f8c91aec8095b8c5ba6d477e
Eddy Lujan <eddylujann@gmail.com>
2025-12-02 17:07:25 +0100

    💾 Pre-Deploy API 2025-12-02 17:07:24

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const corsOptions = {
     : true, // Permitir todo en desarrollo
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'token']
 };
 
 app.use(cors(corsOptions));
